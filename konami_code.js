bb592421cec375f0d85769533989fadec796fb40
Chribtoper <xcstolfax@gmail.com>
2018-07-11 20:17:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,54 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
-  // your code here
+var element = document.createElement('div');
+const main = document.getElementById('main');
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+function keyPressed() {
+  main.addEventListener('keydown');
+}
+
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  // remember all of those fancy DOM node properties?
+  // we're making use of them to get the number
+  // in each div here!
+ 
+  // if `this` is a bit confusing, don't worry —
+  // for now, know that it refers to the div that
+  // is triggering the current event handler.
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('keydown', bubble);
+}
+
+
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
 }
